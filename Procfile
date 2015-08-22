d4daf7566d0b8f13295c037ff89ee4ff8c215799
Joseph Nguyen <vietjew@gmail.com>
2015-08-22 15:48:36 -0700

    Update to follow multiple accounts and run on heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node jokethieves.js -p $PORT
--- jokethieves.js
+++ jokethieves.js
@@ -6,6 +6,15 @@ WATCHED_ACCOUNTS.forEach(function(acct) {
 console.log(watchedAccountsString);
 console.log('app running');
 var Twit = require('twit');
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send("The Fat Jew Hunter will catch you joke thieves!");
+});
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+
 var T = new Twit(require('./config.js'));
 
 function postTweet(message) {
@@ -41,12 +50,6 @@ function processTweet(status) {
   }
 }
 
-// var atMe = T.stream('statuses/filter', {track: '@joenguyentester'});
-// atMe.on('tweet', function(tweet){
-//   console.log(tweet);
-// });
-
-// watch(watchedAccountsString.slice(0,-1));
 function monitorAccount(screenName) {
   var userID;
   T.get('users/lookup', {screen_name: screenName},
@@ -86,78 +89,5 @@ function retweet(tweetID) {
   });
 }
 
-monitorAccount('joenguyentester');
-
-// var testSearch = "the weirdest testing bot at this event right now";
-// var testSearch2 = "I%20forgot%20to%20shower%20before%20this%20dinner%20party%2C%20but%20I%20sat%20near%20the%20cheese%20plate%20so%20it%27s%20all%20good.";
-// var testSearch3 = "the weirdest testing bot at this event right now FINDTHISTWEETOMGDLKFJD"
-// searchTwitter(testSearch3);
-
-// @joenguyentester: '3178995865',
-// @vietjew: '42061404'
-
-
-// { metadata: { iso_language_code: 'en', result_type: 'recent' },
-//   created_at: 'Sat Aug 22 19:41:19 +0000 2015',
-//   id: 635174932649906200,
-//   id_str: '635174932649906176',
-//   text: 'I\'m the weirdest testing bot at this event right now FINDTHISTWEETOMGDLKFJDLKSFJDSKLFJDKLSFJKSDL',
-//   source: '<a href="http://twitter.com" rel="nofollow">Twitter Web Client</a>',
-//   truncated: false,
-//   in_reply_to_status_id: null,
-//   in_reply_to_status_id_str: null,
-//   in_reply_to_user_id: null,
-//   in_reply_to_user_id_str: null,
-//   in_reply_to_screen_name: null,
-//   user:
-//    { id: 3178995865,
-//      id_str: '3178995865',
-//      name: 'Joseph Nguyen',
-//      screen_name: 'joenguyentester',
-//      location: '',
-//      description: '',
-//      url: null,
-//      entities: [Object],
-//      protected: false,
-//      followers_count: 0,
-//      friends_count: 1,
-//      listed_count: 0,
-//      created_at: 'Wed Apr 29 05:30:25 +0000 2015',
-//      favourites_count: 0,
-//      utc_offset: null,
-//      time_zone: null,
-//      geo_enabled: false,
-//      verified: false,
-//      statuses_count: 25,
-//      lang: 'en',
-//      contributors_enabled: false,
-//      is_translator: false,
-//      is_translation_enabled: false,
-//      profile_background_color: 'C0DEED',
-//      profile_background_image_url: 'http://abs.twimg.com/images/themes/theme1/bg.png',
-//      profile_background_image_url_https: 'https://abs.twimg.com/images/themes/theme1/bg.png',
-//      profile_background_tile: false,
-//      profile_image_url: 'http://pbs.twimg.com/profile_images/593286474826547201/Mg4Euv3X_normal.jpg',
-//      profile_image_url_https: 'https://pbs.twimg.com/profile_images/593286474826547201/Mg4Euv3X_normal.jpg',
-//      profile_link_color: '0084B4',
-//      profile_sidebar_border_color: 'C0DEED',
-//      profile_sidebar_fill_color: 'DDEEF6',
-//      profile_text_color: '333333',
-//      profile_use_background_image: true,
-//      has_extended_profile: false,
-//      default_profile: true,
-//      default_profile_image: false,
-//      following: false,
-//      follow_request_sent: false,
-//      notifications: false },
-//   geo: null,
-//   coordinates: null,
-//   place: null,
-//   contributors: null,
-//   is_quote_status: false,
-//   retweet_count: 0,
-//   favorite_count: 0,
-//   entities: { hashtags: [], symbols: [], user_mentions: [], urls: [] },
-//   favorited: false,
-//   retweeted: false,
-//   lang: 'en' }
+// monitorAccount('joenguyentester');
+watchAccount(watchedAccountsString);
